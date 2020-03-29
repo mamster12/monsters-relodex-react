8b0f38e8b2de1e7b53ad49d66e4a857f61ea26ce
unknown <mcvilleanrapsody@gmail.com>
2020-03-29 16:44:05 +0800

    Updates

---
+++ precache-manifest.13bd4283644526508b71e38f9e82085c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf3bf5d2a1c8049997f88bff5542c01",
+    "url": "/monsters-relodex-react/index.html"
+  },
+  {
+    "revision": "598994ff478e32e8929f",
+    "url": "/monsters-relodex-react/static/css/main.843c27d8.chunk.css"
+  },
+  {
+    "revision": "b7025d703d6faac98ebe",
+    "url": "/monsters-relodex-react/static/js/2.a0f84a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex-react/static/js/2.a0f84a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598994ff478e32e8929f",
+    "url": "/monsters-relodex-react/static/js/main.57f5989c.chunk.js"
+  },
+  {
+    "revision": "642d1ef90f67707c8e9e",
+    "url": "/monsters-relodex-react/static/js/runtime-main.7ae3cf13.js"
+  }
+]);
